50dedb06330fa10d408143565e5c21b001ac1f2d
Esteban Palomeque <shaggy.palomeque@gmail.com>
2021-03-31 00:38:24 -0500

    Add img with JSX expression

--- clase2/src/Curso.jsx
+++ clase2/src/Curso.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
+const imgUrl =
+  "https://pi.tedcdn.com/r/talkstar-assets.s3.amazonaws.com/production/playlists/playlist_398/reconnect_with_nature_1200x627.jpg?quality=89&w=1200";
+
 export default function Curso() {
   return (
     <div className="card">
       <div className="card-header">
-        <img
-          src="https://pi.tedcdn.com/r/talkstar-assets.s3.amazonaws.com/production/playlists/playlist_398/reconnect_with_nature_1200x627.jpg?quality=89&w=1200"
-          alt="cover"
-        />
+        <img src={imgUrl} alt="cover" />
       </div>
       <div className="card-body">
         <h3>Title</h3>
