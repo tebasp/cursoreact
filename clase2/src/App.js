7542cd12a5e3ffb93f51487a98edcad92fe096f4
Esteban Palomeque <shaggy.palomeque@gmail.com>
2021-04-01 00:09:12 -0500

    Creando e Iterando cursos

--- clase2/src/App.js
+++ clase2/src/App.js
@@ -1,16 +1,56 @@
 import Curso from "./Curso.jsx";
+import Formulario from "./Formulario";
 import "./styles/styles.scss";
+
+const cursos = [
+  {
+    title: "Node",
+    image:
+      "https://edteam-media.s3.amazonaws.com/courses/medium/7f3ac3ee-a5d5-4ce0-be38-4521dc70b011.png",
+    price: 50,
+    professor: "Esteban Palomeque",
+  },
+  {
+    title: "Java Script",
+    image:
+      "https://edteam-media.s3.amazonaws.com/courses/medium/7f3ac3ee-a5d5-4ce0-be38-4521dc70b011.png",
+    price: 100,
+    professor: "Tebas Palo",
+  },
+  {
+    title: "Vue",
+    image:
+      "https://edteam-media.s3.amazonaws.com/courses/medium/7f3ac3ee-a5d5-4ce0-be38-4521dc70b011.png",
+    price: 600,
+    professor: "Violeta Palomeque",
+  },
+  {
+    title: "React",
+    image:
+      "https://edteam-media.s3.amazonaws.com/courses/medium/7f3ac3ee-a5d5-4ce0-be38-4521dc70b011.png",
+    price: 1,
+    professor: "Janneth Fernandez",
+  },
+];
 
 function App() {
   return (
     <>
-      <h1>This is the title</h1>
       <div className="container">
+        <h1>This is the title</h1>
         <div className="row">
-          <Curso />
-          <Curso />
-          <Curso />
-          <Curso />
+          {cursos.map((curso) => (
+            <Curso
+              title={curso.title}
+              image={curso.image}
+              price={curso.price}
+              professor={curso.professor}
+            />
+          ))}
+        </div>
+
+        <div>
+          <Formulario />
         </div>
       </div>
     </>
--- clase2/src/Curso.jsx
+++ clase2/src/Curso.jsx
@@ -1,18 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const imgUrl =
-  "https://pi.tedcdn.com/r/talkstar-assets.s3.amazonaws.com/production/playlists/playlist_398/reconnect_with_nature_1200x627.jpg?quality=89&w=1200";
-
-export default function Curso() {
+export default function Curso({ title, image, price, professor }) {
   return (
     <div className="card">
       <div className="card-header">
-        <img src={imgUrl} alt="cover" />
+        <img src={image} alt={title} />
       </div>
       <div className="card-body">
-        <h3>Title</h3>
-        <p>Some text</p>
+        <h3 className="card-title">{title}</h3>
+        <p className="card-professor">{professor}</p>
+        <code className="card-price">{price}</code>
       </div>
     </div>
   );
 }
+
+Curso.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.number,
+  professor: PropTypes.string,
+};
+
+Curso.defaultProps = {
+  title: "Titulo",
+  image:
+    "https://pi.tedcdn.com/r/talkstar-assets.s3.amazonaws.com/production/playlists/playlist_398/reconnect_with_nature_1200x627.jpg?quality=89&w=1200",
+  price: 0,
+  professor: "",
+};
