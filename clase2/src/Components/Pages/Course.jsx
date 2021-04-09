6892762a0d3aa15959a03659463e035abafbef42
Esteban Palomeque <shaggy.palomeque@gmail.com>
2021-04-09 00:28:11 -0500

    Call custom hook in Course component

--- clase2/src/Components/Pages/Course.jsx
+++ clase2/src/Components/Pages/Course.jsx
@@ -1,56 +1,42 @@
 import React from "react";
-
-const cursos = [
-  {
-    id: 1,
-    title: "Node",
-    image:
-      "https://edteam-media.s3.amazonaws.com/courses/medium/7f3ac3ee-a5d5-4ce0-be38-4521dc70b011.png",
-    price: 50,
-    professor: "Esteban Palomeque",
-  },
-  {
-    id: 2,
-    title: "Java Script",
-    image:
-      "https://edteam-media.s3.amazonaws.com/courses/medium/7f3ac3ee-a5d5-4ce0-be38-4521dc70b011.png",
-    price: 100,
-    professor: "Tebas Palo",
-  },
-  {
-    id: 3,
-    title: "Vue",
-    image:
-      "https://edteam-media.s3.amazonaws.com/courses/medium/7f3ac3ee-a5d5-4ce0-be38-4521dc70b011.png",
-    price: 600,
-    professor: "Violeta Palomeque",
-  },
-  {
-    id: 4,
-    title: "React",
-    image:
-      "https://edteam-media.s3.amazonaws.com/courses/medium/7f3ac3ee-a5d5-4ce0-be38-4521dc70b011.png",
-    price: 1,
-    professor: "Janneth Fernandez",
-  },
-];
+import useCourse from "../customHooks/useCourse";
 
 const Course = ({ match }) => {
   // [0] por que filter retorna un array
-  const currentCourse = cursos.filter(
-    (curso) => curso.id === parseInt(match.params.id)
-  )[0];
+  // const currentCourse = cursos.filter(
+  //   (curso) => curso.id === parseInt(match.params.id)
+  // )[0];
 
-  console.log("curso", currentCourse);
+  // const [state, setState] = useState({
+  //   id: 1,
+  //   title: "Node",
+  //   image:
+  //     "https://edteam-media.s3.amazonaws.com/courses/medium/7f3ac3ee-a5d5-4ce0-be38-4521dc70b011.png",
+  //   price: 50,
+  //   professor: "Esteban Palomeque",
+  // });
+
+  // const changeTitle = (title) => {
+  //   setState({
+  //     ...state,
+  //     title,
+  //   });
+  // };
+
+  const course = useCourse(match.params.id) || [];
 
   return (
     <div className="container">
       <div className="row">
-        <h2>{currentCourse.title}</h2>
-        <img src={currentCourse.image} alt={currentCourse.title} />
-        <div>{currentCourse.professor}</div>
-        <div>{currentCourse.price}</div>
+        <h2>{course.title}</h2>
+        <img src={course.image} alt={course.title} />
+        <div>{course.professor}</div>
+        <div>{course.price}</div>
       </div>
+
+      {/* <button onClick={changeTitle.bind(this, "New title")}>
+        Change title
+      </button> */}
     </div>
   );
 };
